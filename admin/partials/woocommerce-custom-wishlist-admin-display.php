1da3961c9439b2a3e27ef9c910e15b5edaa3dc4e
Basantasolivatech <basanta@solivatech.com>
2024-02-07 13:38:52 +0530

    woo wish list added

---
+++ admin/partials/woocommerce-custom-wishlist-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://digiwebcam.in/
+ * @since      1.0.0
+ *
+ * @package    Woocommerce_Custom_Wishlist
+ * @subpackage Woocommerce_Custom_Wishlist/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
